54583dcf6924d49a3207cb041cc042e331b7fb25
Vishal-HFN <vishal.prakash@volunteer.heartfulness.org>
2023-05-02 23:52:55 +0530

    strapi cloudinary and render file setup

---
+++ config/server.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: env('CLOUDINARY_NAME'),
+      api_key: env('CLOUDINARY_KEY'),
+      api_secret: env('CLOUDINARY_SECRET'),
+    },
+  },
+});
