ff12379ed8f286574f11f7794986111685f2cc81
Vishal-HFN <vishal.prakash@volunteer.heartfulness.org>
2023-05-07 00:23:45 +0530

    created app folder

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', 'https://rakshai-strapi.onrender.com'),
   port: env.int('PORT', 1337),
   url: "https://rakshai-strapi.onrender.com",
   app: {
