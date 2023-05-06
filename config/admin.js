a295f87f1deba51c8ae506977c512244df438ff9
Vishal-HFN <vishal.prakash@volunteer.heartfulness.org>
2023-05-06 14:05:51 +0530

    added disk in render n url in server n admin conf

--- config/admin.js
+++ config/admin.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
+  url: "https://rakshai-strapi.onrender.com/admin",
   apiToken: {
     salt: env('API_TOKEN_SALT'),
   },
